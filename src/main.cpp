de6cdb62348169e0fdcd3110e4d6ba0e2127d79d
9937035-max <9937035@schoolsnet.act.edu.au>
2026-08-11 11:39:52 +1000

    got hex codes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vehicle.h>
 #include <ultrasonic.h>
 #include <ESP32Servo.h>
+#include <IRremote.h>
+
 
 vehicle myCar;
 Servo myServo;
@@ -9,97 +11,82 @@ ultrasonic myUltrasonic;
 int UT_distance=0;
 Servo myServou;
 
+
 int myservo_Pin=25;
 int leftDistance=0;
 int middleDistance=0;
 int rightDistance=0;
-
+#define IRpin 4
 #define leftLED 2
 #define rightLED 12
 #define antiClockwise Contrarotate
-#define buzzer 33
 #define servopin 25
 #define sonarmoter 25
 
-void buzzersound()
-{
-  digitalWrite(buzzer, 255);
-  delay(1000);
-  digitalWrite(buzzer, 000);
-  delay(1000);
-  
-}
+IRrecv myIRrev(IRpin);
 
 void forward()
 {
-  myCar.Move(Forward, 255);
-  delay(1000);
+  myCar.Move(Forward, 155);
 }
 
 void backward()
 {
-  myCar.Move(Backward, 255);
-  delay(1000);
+  myCar.Move(Backward, 155);
 }
 
 void rotateLeft()
 {
-  myCar.Move(antiClockwise, 255);
+  myCar.Move(antiClockwise, 155);
     digitalWrite(leftLED, HIGH);
-  delay(1000);
   digitalWrite(leftLED, LOW);
 }
 
 void rotateRight()
 {
-  myCar.Move(Clockwise, 255);
+  myCar.Move(Clockwise, 155);
   digitalWrite(rightLED, HIGH);
-  delay(1000);
     digitalWrite(rightLED, LOW);
 }
 
 void strafeLeft()
 {
-  myCar.Move(Move_Left, 255);
-  delay(1000);
+  myCar.Move(Move_Left, 155);
 }
 
 void strafeRight()
 {
-  myCar.Move(Move_Right, 255);
-  delay(1000);
+  myCar.Move(Move_Right, 155);
 }
 
 void setup()
 {
- Serial.begin(115200);
+ Serial.begin(115000);
 pinMode(leftLED, OUTPUT);
 pinMode(rightLED, OUTPUT);
-pinMode(buzzer, OUTPUT);
 pinMode(sonarmoter, OUTPUT);
+pinMode(IRpin, INPUT);
 myUltrasonic.Init(13, 14);
 myCar.Init();
 myServo.attach(servopin);
 myServo.write(0);
-
-
+myIRrev.enableIRIn();
 }
-
 void loop()
 {
   myServo.write(100);
-
   if (myUltrasonic.Ranging() < 100)
   {
-   rotateLeft();
+   rotateRight();
   }
   else
   {
     forward();
   }
-  
-  
-
-
-    
+  delay(100);
+  if (myIRrev.decode())
+  {
+    Serial.println(myIRrev.decodedIRData.decodedRawData, HEX);
+    myIRrev.resume();
+  }
 }
