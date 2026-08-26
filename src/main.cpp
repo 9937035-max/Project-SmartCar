d56677848a532dcc553e4e01393426e37ad58c83
9937035-max <9937035@schoolsnet.act.edu.au>
2026-08-27 09:37:11 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,10 @@ ultrasonic myUltrasonic;
 int UT_distance=0;
 Servo myServou;
 
-
 int myservo_Pin=25;
 int leftDistance=0;
 int middleDistance=0;
 int rightDistance=0;
-int Speed=155
 #define IRpin 4
 #define leftLED 2
 #define rightLED 12
@@ -63,11 +61,6 @@ void strafeLeft()
 void strafeRight()
 {
   myCar.Move(Move_Right, 155);
-}
-
-void topSpeed()
-{
-Speed=255
 }
 
 void roamingMode()
@@ -120,24 +113,22 @@ current_decode = last_decode;
 Serial.print(current_decode, HEX);
 Serial.println("");
 switch (current_decode) {
-case 0xB946FF00: myCar.Move(Forward, Speed); break;
+case 0xB946FF00: myCar.Move(Forward, 155); break;
 // Press "up" button to move forward
-case 0xEA15FF00: myCar.Move(Backward, Speed); break;
+case 0xEA15FF00: myCar.Move(Backward, 155); break;
 // Press "down" button to move backward
-case 0xBB44FF00: myCar.Move(Contrarotate, Speed); break;
+case 0xBB44FF00: myCar.Move(Contrarotate, 155); break;
 // Press "left" button to turn left
-case 0xBC43FF00: myCar.Move(Clockwise, Speed); break;
+case 0xBC43FF00: myCar.Move(Clockwise, 155); break;
 // Press "right" button to turn right
-case 0xE916FF00: myCar.Move(Move_Left, Speed); break;
+case 0xE916FF00: myCar.Move(Move_Left, 155); break;
 // Press button "1" to move left
-case 0xF20DFF00: myCar.Move(Move_Right, Speed); break;
+case 0xF20DFF00: myCar.Move(Move_Right, 155); break;
 // Press button "3" to move right
 case 0xBF40FF00: myCar.Move(Stop, 0); break;
 // Press button "OK" to stop
 case 0xB54AFF00: roamingMode(); 
 // Press button "#" starts roming mode
-case 0xAD52FF00: topSpeed(); break;
-// Press button "0" make the smart car quicker (isnt undo able)
 }
 myIRrev.resume(); // Wait for the next IR signal
  }
